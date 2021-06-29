50e94f0aeff6ae24c8950251bbf39cf2d5d8e35a
Craig Miller <craigmiller160@gmail.com>
2021-06-29 13:54:17 -0400

    Removed JS files that should never have been generated

--- src/logger/index.js
+++
@@ -1,9 +0,0 @@
-var _a;
-import pino from 'pino';
-var logger = pino({
-    level: (_a = process.env.LOGGER_LEVEL) !== null && _a !== void 0 ? _a : 'info',
-    prettyPrint: {
-        translateTime: 'yyyy-MM-dd HH:mm:ss.l'
-    }
-});
-export default logger;
