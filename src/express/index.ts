207b10fb0e88ad09dd973f665ca2ae7f177e4fbc
Craig Miller <craigmiller160@gmail.com>
2021-06-29 13:45:19 -0400

    Ran linter

--- src/express/index.ts
+++ src/express/index.ts
@@ -5,15 +5,15 @@ import logger from '../logger';
 
 const app = express();
 if (!process.env.PORT) {
-    throw new Error('Must set PORT environment variable');
+	throw new Error('Must set PORT environment variable');
 }
 const port = parseInt(process.env.PORT, 10);
 
-const startExpressServer = () => {
-    const server = https.createServer(tlsProps, app);
-    server.listen(port, () => {
-        logger.info(`Express server running on port ${port}`);
-    });
+const startExpressServer = (): void => {
+	const server = https.createServer(tlsProps, app);
+	server.listen(port, () => {
+		logger.info(`Express server running on port ${port}`);
+	});
 };
 
 export default startExpressServer;
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,10 +1,10 @@
 import pino from 'pino';
 
 const logger = pino({
-    level: process.env.LOGGER_LEVEL ?? 'info',
-    prettyPrint: {
-        translateTime: 'yyyy-MM-dd HH:mm:ss.l'
-    }
+	level: process.env.LOGGER_LEVEL ?? 'info',
+	prettyPrint: {
+		translateTime: 'yyyy-MM-dd HH:mm:ss.l'
+	}
 });
 
 export default logger;
