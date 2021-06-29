0d03b81e653dc2b35bb7be73b7c333a51cd05861
Craig Miller <craigmiller160@gmail.com>
2021-06-29 15:53:45 -0400

    More progress on MongoDB setup

--- src/mongo/connect.ts
+++ src/mongo/connect.ts
@@ -1,8 +1,19 @@
-import { MongoClient } from 'mongodb';
+import {Db, MongoClient} from 'mongodb';
 import handleMongoPasswordEnv from './handleMongoPasswordEnv';
 import logger from '../logger';
 import * as TE from 'fp-ts/TaskEither';
+import * as E from 'fp-ts/Either';
 import handleUnknownError from '../utils/handleUnknownError';
+import {pipe} from 'fp-ts/function';
+
+handleMongoPasswordEnv();
+
+// TODO still work on Handler type
+export type HandlerType<R> = (db: Db) => R;
+export interface HandlerResult<R> {
+    client: MongoClient;
+    result: R;
+}
 
 const getConfig = () => ({
     mongoUser: process.env.MONGO_USER,
@@ -29,8 +40,23 @@ const buildMongoConnectionString = () => {
     return `mongodb://${credsString}${coreConnectString}${tlsString}`;
 };
 
-// TODO work on handler type
-const connect = (handler: any) => {
+const execute = <R>(client: MongoClient, handler: HandlerType<R>): TE.TaskEither<Error, HandlerResult<R>> => {
+    const db = client.db();
+    return pipe(
+        E.tryCatch(
+            () => handler(db),
+            handleUnknownError
+        ),
+        TE.fromEither,
+        TE.map((result: R) => ({
+            client,
+            result
+        }))
+    );
+};
+
+// TODO need better return type here
+const connect = <R>(handler: HandlerType<R>): void => {
     const connectString = buildMongoConnectionString();
     const options = {
         useUnifiedTopology: true
@@ -38,8 +64,14 @@ const connect = (handler: any) => {
 
     logger.debug(`Opening MongoDB connection: ${connectString}`);
 
-    TE.tryCatch(
-        () => MongoClient.connect(connectString, options),
-        handleUnknownError
+    // TODO need to close the client
+    pipe(
+        TE.tryCatch(
+            () => MongoClient.connect(connectString, options),
+            handleUnknownError
+        ),
+        TE.chain((client: MongoClient) => execute(client, handler))
     );
 };
+
+export default connect;
